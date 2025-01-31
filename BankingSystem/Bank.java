dea1b065995e5d821b5d4d73d8ea5d5f6a43795b
Amir Abaskanov <absknv@gmail.com>
2025-01-31 17:02:28 -0500

    Added the Bank Class

--- BankingSystem/Bank.java
+++ BankingSystem/Bank.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
-    
+    private List<Account> accounts;
+
+    //Constructor
+    public Bank() {
+        accounts = new ArrayList<>();
+    }
+
+    //Add a new account
+    public void addAccount(Account account){
+        accounts.add(account);
+        System.out.println("Account added successfully.");
+    }
+
+    //Find an account by account number
+    public Account findAccount(String accountNumber){
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    //Display all accounts
+    public void displayAllAccounts(){
+        for (Account account : accounts){
+            account.checkBalance();
+            System.out.println("------------------");
+        }
+    }
 }
